abf46c5014775f513cb75fc116ad23d30521bbc6
PM2D <m2d@sibnet.ru>
2010-01-07 14:21:34 +0700

    temporary fix ratings with some wrong \(numeric\) keys

--- rate.php
+++ rate.php
@@ -2,6 +2,7 @@
 require($_SERVER['DOCUMENT_ROOT'].'/etc/main.php');
 // получение и обработка данных (ключ, оценка, куда возвращаться)
 $key = isset($_POST['key']) ? $_POST['key'] : getvar('key');
+$key = trim($key);
 $rate = isset($_POST['rate']) ? intval($_POST['rate']) : intval(getvar('rate'));
 $back = isset($_POST['back']) ? $_POST['back'] : getvar('back');
 // если не было указано куда возвращаться пробуем реферер иначе возвращаем на главную
@@ -12,7 +13,9 @@ if(empty($back)) {
 if(3 == $USER['id']) raise_error('Извините, но незарегистрированные пользователи не могут учавствовать в оценках.', $back);
 // если оценки нет, то добавляем ее
 $rating = new rating;
-if(!empty($key)) $rating->SetKey($key);
+// !is_numerik($key) - временный костыль,
+// т.к. в будущем необходимо будет поправить шаблоны в которых ключ передается неверно
+if(!empty($key) && !is_numeric($key)) $rating->SetKey($key);
 if(TRUE === $rating->Rate($rate)) {
   Header('Location: '.$back, TRUE, 302);
 } else {
