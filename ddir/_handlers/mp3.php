d2fe09d10219531048e686ef5d493b2762168fcd
DreamDragon <m2d@sibnet.ru>
2012-11-05 20:43:15 +0700

    Fix loading of mp3 class and cutting mp3 files in ddir

--- ddir/_handlers/mp3.php
+++ ddir/_handlers/mp3.php
@@ -10,45 +10,58 @@ $file = $mysql->GetRow('*', 'files', '`id`='.$id);
 
 if(!file_exists($_SERVER['DOCUMENT_ROOT'].'/ddir'.$file['path'])) raise_error('Heт тaкoгo фaйлa.');
 
+// spl не подгружает классы заканчивающиеся на цифру
+// подгружаем этот класс вручную в качестве исключения
+require($_SERVER['DOCUMENT_ROOT'].'/lib/mp3.php');
+
 $mp3 = new mp3;
 $data = $mp3->get_mp3($_SERVER['DOCUMENT_ROOT'].'/ddir'.$file['path'], FALSE);
-if(!isset($data['id3v1'])) $data = $mp3->get_mp3($_SERVER['DOCUMENT_ROOT'].'/ddir'.$file['path'], TRUE);
 
-if(isset($_GET['start']) && isset($_GET['end'])){
+if ( !isset($data['id3v1']) ) $data = $mp3->get_mp3($_SERVER['DOCUMENT_ROOT'].'/ddir'.$file['path'], TRUE);
+
+if ( isset($_GET['start']) && isset($_GET['end']) )
+{
 
   // сборщик "мусора" (более 10 минут == мусор)
   $d = opendir($_SERVER['DOCUMENT_ROOT'].'/tmp/mp3');
-  while($str = readdir($d)){
-   if($str[0]!='.' && filemtime($_SERVER['DOCUMENT_ROOT'].'/tmp/mp3/'.$str)<($TIME-600)){
+  while ( $str = readdir($d) )
+  {
+    if ( $str[0]!='.' && filemtime($_SERVER['DOCUMENT_ROOT'].'/tmp/mp3/'.$str)<($TIME-600) )
+    {
      unlink($_SERVER['DOCUMENT_ROOT'].'/tmp/mp3/'.$str);
-   }
+    }
   }
 
   $start = intval($_GET['start']);
   $end = intval($_GET['end']);
-  if($start==$end) raise_error('Начало это конец? О_о', 'mp3.php?f='.$id.'&amp;'.SID);
-  if($start>$data['data']['length']) raise_error('Начало больше длинны? О_о', 'mp3.php?f='.$id.'&amp;'.SID);
-  if(0>$end) $end = 1;
-  if(0>$start) $start = 0;
+  if ( $start == $end ) raise_error('Начало это конец? О_о', 'mp3.php?f='.$id.'&amp;'.SID);
+  if ( $start > $data['data']['length'] ) raise_error('Начало больше длинны? О_о', 'mp3.php?f='.$id.'&amp;'.SID);
+  if ( 0 > $end ) $end = 1;
+  if ( 0 > $start ) $start = 0;
   $end = min($end, $data['data']['length']);
-  $tmpname = basename($path);
-  if($mp3->cut_mp3($_SERVER['DOCUMENT_ROOT'].'/ddir'.$file['path'], $_SERVER['DOCUMENT_ROOT'].'/tmp/mp3/'.$tmpname, $start, $end, 'second', FALSE)){
+  $tmpname = basename($file['path']);
+
+  if ( $mp3->cut_mp3($_SERVER['DOCUMENT_ROOT'].'/ddir'.$file['path'], $_SERVER['DOCUMENT_ROOT'].'/tmp/mp3/'.$tmpname, $start, $end, 'second', FALSE) )
+  {
     Header('Location: /tmp/mp3/'.$tmpname); exit;
-  } else {
+  }
+  else
+  {
     raise_error('Не удалось обрезать mp3-файл.', 'mp3.php?f='.$id.'&amp;'.SID);
   }
+
 }
 
 $tmpl = new template;
 $tmpl->Vars['TITLE'] = $file['name'];
 
-if($data['id3v1']['title']) $tmpl->Vars['PROPS'][] = array('title'=>'Haзвaниe', 'value'=>$data['id3v1']['title']);
-if($data['id3v1']['artist']) $tmpl->Vars['PROPS'][] = array('title'=>'Иcпoлнитeль', 'value'=>$data['id3v1']['artist']);
-if($data['id3v1']['album']) $tmpl->Vars['PROPS'][] = array('title'=>'Aльбoм', 'value'=>$data['id3v1']['album']);
-if(trim($data['id3v1']['year'])) $tmpl->Vars['PROPS'][] = array('title'=>'Гoд', 'value'=>$data['id3v1']['year']);
-if($data['id3v1']['track']) $tmpl->Vars['PROPS'][] = array('title'=>'Тpeк', 'value'=>$data['id3v1']['track']);
-if($data['id3v1']['genre']) $tmpl->Vars['PROPS'][] = array('title'=>'Жанр', 'value'=>$data['id3v1']['genre']);
-if(trim($data['id3v1']['comment'])) $tmpl->Vars['PROPS'][] = array('title'=>'Koммeнтapий', 'value'=>$data['id3v1']['comment']);
+if ( $data['id3v1']['title'] ) $tmpl->Vars['PROPS'][] = array('title'=>'Haзвaниe', 'value'=>$data['id3v1']['title']);
+if ( $data['id3v1']['artist'] ) $tmpl->Vars['PROPS'][] = array('title'=>'Иcпoлнитeль', 'value'=>$data['id3v1']['artist']);
+if ( $data['id3v1']['album'] ) $tmpl->Vars['PROPS'][] = array('title'=>'Aльбoм', 'value'=>$data['id3v1']['album']);
+if ( trim($data['id3v1']['year']) ) $tmpl->Vars['PROPS'][] = array('title'=>'Гoд', 'value'=>$data['id3v1']['year']);
+if ( $data['id3v1']['track'] ) $tmpl->Vars['PROPS'][] = array('title'=>'Тpeк', 'value'=>$data['id3v1']['track']);
+if ( $data['id3v1']['genre'] ) $tmpl->Vars['PROPS'][] = array('title'=>'Жанр', 'value'=>$data['id3v1']['genre']);
+if ( trim($data['id3v1']['comment']) ) $tmpl->Vars['PROPS'][] = array('title'=>'Koммeнтapий', 'value'=>$data['id3v1']['comment']);
 
 $tmpl->Vars['PROPS'][] = array('title'=>'Битpeйт', 'value'=>$data['data']['bitrate'].'kbps');
 $tmpl->Vars['PROPS'][] = array('title'=>'Чacтoтa', 'value'=>$data['data']['sampling_frequency'].'Hz');
@@ -64,6 +77,7 @@ $about = dirname($path).'/_'.basename($path, '.mp3').'.txt';
 $tmpl->Vars['ABOUT'] = file_exists($about) ? file_get_contents($about)) : FALSE;
 */
 
+$tmpl->Vars['FILEID'] = $id;
 $tmpl->Vars['PATH'] = '/ddir'.$file['path'];
 $tmpl->Vars['BACK'] = '/ddir/?d='.$file['did'].'&amp;'.SID;
 // соседние файлы
