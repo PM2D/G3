6b8f3e4156bf0415102d163b8dac3ddabb00dac5
PM2D <m2d@sibnet.ru>
2012-11-06 13:39:20 +0700

    Fix guest access to write comments in filex

--- filex/comms.php
+++ filex/comms.php
@@ -18,7 +18,8 @@ $tmpl->SendHeaders();
 $compress->Enable();
 $tmpl->Vars['TITLE'] = 'Комментарии к файлу';
 
-if(isset($_POST['comm'])){
+if ( isset($_POST['comm']) )
+{
 
   $tmpl->Vars['TITLE'] = 'Добавление комментария';
   $in['id'] = 0;
@@ -28,10 +29,14 @@ if(isset($_POST['comm'])){
   $in['uid'] = $USER['id'];
   $in['time'] = $TIME;
   $in['msg'] = stripslashes(htmlspecialchars($_POST['comm']));
-  if(3==$USER['id'] && $_SESSION['code']!=postvar('code')){
-    raise_error('Вы ввeли нeвepный кoд пoдтвepждeния', 'comms.php?f='.$fid.'&amp;'.SID);
-  };
-  if(!trim($in['msg'])) raise_error('Вы не заполнили поле.', 'comms.php?f='.$fid.'&amp;'.SID);
+  if ( 3==$USER['id'] )
+  {
+    if ( !$CFG['FILEX']['guests'] )
+      raise_error('Извините, но возможность добавления комментариев неавторизованными пользователями была отключена администратором.', 'comms.php?f='.$fid.'&amp;'.SID);
+    elseif ( $_SESSION['code']!=postvar('code') )
+      raise_error('Вы ввeли нeвepный кoд пoдтвepждeния', 'comms.php?f='.$fid.'&amp;'.SID);
+  }
+  if ( !trim($in['msg']) ) raise_error('Вы не заполнили поле.', 'comms.php?f='.$fid.'&amp;'.SID);
   $smiles = new smiles;
   $smiles->ToImg($in['msg']);
   $tags = new tags;
@@ -39,25 +44,28 @@ if(isset($_POST['comm'])){
   $in['msg'] = preg_replace('/(&[a-z]{2,4})</', '$1;<', $in['msg']);
   $in['msg'] = nl2br($in['msg']);
   $in['msg'] = $mysql->EscapeString($in['msg']);
-  if($mysql->Insert('filex_comms', $in)){
+  if ( $mysql->Insert('filex_comms', $in) )
+  {
     $mysql->Update('filex_files', array('comms'=>'`comms`+1'), '`id`='.$file['id'].' LIMIT 1');
     $tmpl->Vars['MESSAGE'] = $USER['login'].', вaш кoммeнтapий дoбaвлeн';
     $tmpl->Vars['BACK'] = 'comms.php?f='.$fid.'&amp;'.SID;
     echo $tmpl->Parse('notice.tmpl');
-  };
+  }
   exit;
 
-};
+}
 
 $mysql->Query('SELECT SQL_CALC_FOUND_ROWS `filex_comms`.*,`users`.`login`
 FROM `filex_comms` LEFT JOIN `users` ON `filex_comms`.`uid`=`users`.`id`
 WHERE `filex_comms`.`fid`='.$fid.' ORDER BY `filex_comms`.`time` LIMIT '.$n.','.($USER['np']+1));
 
 $tmpl->Vars['COMMS'] = array();
-while($arr = $mysql->FetchAssoc()){
+while ( $arr = $mysql->FetchAssoc() )
+{
   $tmpl->Vars['COMMS'][] = $arr;
 }
 
+$tmpl->Vars['GUESTS'] = $CFG['FILEX']['guests'];
 $tmpl->UseNav();
 $tmpl->Vars['NAV']['pos'] = $n;
 $tmpl->Vars['NAV']['total'] = $mysql->GetFoundRows();
